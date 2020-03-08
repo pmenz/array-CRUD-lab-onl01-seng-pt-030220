6abca1121027916a1f355a246b0e3e6eeadf8aa9
Pablo Menz <p.menz.r@gmail.com>
2020-03-08 21:12:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,17 +19,18 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["I", "am", "really", "learning","arrays!"]
+  array = ["wow","I", "am", "really", "learning","arrays!"]
   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["I", "am", "really", "learning"]
+  array = ["wow","I", "am", "really", "learning","arrays!"]
   array.pop
 end
 
 def retrieve_element_from_index(array, index_number)
   array = ["I", "am", "really", "learning"]
+  array.index(1)
 end
 
 def retrieve_first_element_from_array(array)
