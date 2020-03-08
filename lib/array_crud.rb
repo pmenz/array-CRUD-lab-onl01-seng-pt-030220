61dcc0421a2ce1c6ff50ee2e32191b368d83c06d
Pablo Menz <p.menz.r@gmail.com>
2020-03-08 21:06:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["I", "am", "really", "learning"]
-  element = "wow"
+  element = "arrays"
 end
 
 def add_element_to_start_of_array(array, element)
