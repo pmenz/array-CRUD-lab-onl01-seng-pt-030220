17ac035d77b689182df36087065d6578c3c78ec6
Pablo Menz <p.menz.r@gmail.com>
2020-03-08 21:26:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,8 +30,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow","I", "am", "really", "learning","arrays!"]
-  index_number = 2
-  array(index_number)
+  array(2)
 end
 
 def retrieve_first_element_from_array(array)
